8fd20ff849dc7d762eb9370d9a3ddc3e6ba702bf
이정우 <ljw3771@kakao.com>
2022-11-17 14:29:14 +0900

    과제 3번까지 완료시점

--- app.js
+++ app.js
@@ -50,6 +50,47 @@ const httpRequestListener = function (req, res) {
       res.end(JSON.stringify(posts));
     }
   }
+  if (method === 'POST') {
+    if (url === '/user') {
+      let body = '';
+      req.on('data', (data) => {
+        body += data;
+      });
+
+      req.on('end', () => {
+        const user = JSON.parse(body);
+
+        users.push({
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          password: user.password,
+        });
+        res.writeHead(201, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ message: 'userCreated' }));
+      });
+    }
+
+    if (url === '/posts') {
+      let body = '';
+      req.on('data', (data) => {
+        body += data;
+      });
+
+      req.on('end', () => {
+        const post = JSON.parse(body);
+        posts.push({
+          id: post.id,
+          title: post.title,
+          content: post.content,
+          userId: post.userId,
+        });
+        console.log(posts);
+        res.writeHead(201, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ message: 'postCreated' }));
+      });
+    }
+  }
 };
 
 server.on('request', httpRequestListener);
